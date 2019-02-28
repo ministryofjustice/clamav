4860711a2d5cd2782583425a3091fd50dd8b5574
Julien Sansot <juliensansot@gmail.com>
2019-02-28 11:12:37 +0000

    init repo

---
+++ README.md
@@ -0,0 +1,21 @@
+# clamav
+
+ClamAV daemon as a Docker image. It *builds* with a current virus database and
+*runs* `freshclam` in the background constantly updating the virus signature database. `clamd` itself
+is listening on exposed port `3310`.
+
+On your main container, you still need to install `clamav-daemon` :
+
+`apt-get install clamav-daemon -y`
+
+Then you can scan a file with: `clamdscan -c clamd.container.conf --stream file`
+
+clamd.container.conf being:
+```
+TCPSocket 3310
+TCPAddr localhost # or wherever you setup this container
+```
+
+---
+
+copied from https://github.com/mko-x/docker-clamav
---
+++ bootstrap.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# bootstrap clam av service and clam av database updater shell script
+# copied from https://github.com/mko-x/docker-clamav
+
+set -m
+
+# start clam service itself and the updater in background as daemon
+freshclam -d &
+clamd &
+
+# recognize PIDs
+pidlist=`jobs -p`
+
+# initialize latest result var
+latest_exit=0
+
+# define shutdown helper
+function shutdown() {
+    trap "" SIGINT
+
+    for single in $pidlist; do
+        if ! kill -0 $single 2>/dev/null; then
+            wait $single
+            latest_exit=$?
+        fi
+    done
+
+    kill $pidlist 2>/dev/null
+}
+
+# run shutdown
+trap shutdown SIGINT
+wait -n
+
+# return received result
+exit $latest_exit
